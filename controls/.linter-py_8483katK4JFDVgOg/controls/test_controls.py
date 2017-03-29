44a1469cf2e8139d288f5e042a101771bde8c899
Ahmedsebit <Ahmed.yusuf@andela.com>
2017-03-29 15:38:17 +0300

    Checking for tests failure

--- controls/.linter-py_8483katK4JFDVgOg/controls/test_controls.py
+++ controls/.linter-py_8483katK4JFDVgOg/controls/test_controls.py
@@ -9,81 +9,80 @@ class AmityControlTest(TestCase):
         self.amity = AmityControls()
 
     def test_if_room_exists(self):
-        self.assertFalse('K' in self.amity.all_rooms, msg="Room already exist")
+        '''Test for searching if room exists'''
+        self.assertTrue('K' in self.amity.all_rooms, msg="Room does not exist")
 
 
     def test_create_room(self):
+        '''Test for succesfull room created'''
         CurrentCount = len(self.amity.all_rooms)
-        self.amity.create_room('Adis Ababa', 'Office')
+        self.amity.create_room('Kigali', 'Office')
         NewCount = len(self.amity.all_rooms)
         self.assertEqual(CurrentCount + 1, NewCount, msg="Room not created")
 
     def test_invalid_roomtype(self):
-        self.amity.create_room('Adis Ababa', 'Office')
-        self.assertEqual(self.amity.all_rooms['Adis Ababa'], "Office", msg="Invalid Type")
+        '''Test for checking the created room type'''
+        self.amity.create_room('Adis Ababa', 'Off')
+        self.assertEqual(self.amity.all_rooms['Adis Ababa'], "Office", msg="Invalid Room Type")
 
     def test_if_person_exist(self):
-        self.assertFalse('A' in self.amity.all_allocations, msg="Person already exists")
+        '''Test for searching if person exists'''
+        self.assertTrue('A' in self.amity.all_allocations, msg="Person doe not exists")
 
 
     def test_add_person(self):
+        '''Test for succesfull person created'''
         CurrentCount = len(self.amity.all_persons)
-        self.amity.add_person('Sebit', 'Rajab', 'Fellow', 'Y')
+        self.amity.add_person('Ahmed', 'Yusuf', 'Fellow', 'Y')
         NewCount = len(self.amity.all_persons)
         self.assertEqual(CurrentCount + 1, NewCount, msg="Person not added")
 
     def test_allocate_room(self):
+        '''Test for succesfull room allocation'''
         CurrentSpaces = len(self.amity.all_allocations)
-        self.amity.add_person('T', 'N','Fellow', 'Y')
+        self.amity.add_person('Ahmed', 'Yusuf','Fellow', 'Y')
         NewSpaces = len(self.amity.all_allocations)
         self.assertEqual(CurrentSpaces + 1, NewSpaces, msg="Room not allocated")
 
     def test_reallocate_office(self):
+        '''Test for succesfull room relocation'''
         old_location = self.amity.all_allocations['Ahmed Yusuf'][1]
         self.amity.relocate_office('Ahmed', 'Yusuf','Office', 'Adis Ababa')
         new_location = self.amity.all_allocations['Ahmed Yusuf'][1]
         self.assertNotEqual(old_location, new_location, msg="Relocation unsuccesfull")
 
     def test_allocate_staff_living_space(self):
-        self.amity.add_person('T', 'S', 'Fellow', 'Y')
+        '''Test for wrong living space allocation'''
+        self.amity.add_person('T', 'S', 'Staff', 'Y')
         self.assertNotEqual(self.amity.all_persons['T S'], 'Staff', msg="Cannot allocate staff a room")
 
-    def test_invalid_input_person(self):
-        #self.amity.add_person("1", "2", 3, 'Y')
-        #person = self.amity.all_persons[1]
-        #self.assertTrue(TypeError)
-        pass
+    # def test_invalid_input_person(self):
+    #     #self.amity.add_person("1", "2", 3, 'Y')
+    #     #person = self.amity.all_persons[1]
+    #     #self.assertTrue(TypeError)
+    #     pass
 
     def test_invalid_input_room(self):
-        self.amity.create_room(1, "2")
+        '''Test for wrong room input'''
+        self.amity.create_room(1, 2)
         room = self.amity.all_rooms[1]
         self.assertFalse(isinstance(room, int), msg="Invalid input")
 
     def test_load_people(self):
-        CurrentCount = len(self.amity.all_allocations)
-        f = open('/Users/ahmedyusuf/desktop/CP1AOfficeSpaceAllocation/officespaceallocation/files/person.txt','r')
-        count = 0
-        for line in f:
-            item = line.split()
-            self.amity.add_person(item[0], item[1], item[2], item[3])
-            count +=1
-        NewCount = len(self.amity.all_allocations)
-        self.assertEqual(CurrentCount + count, NewCount, msg="None")
+        '''Test for succesfull room created'''
+        CurrentSpaces = len(self.amity.all_allocations)
+        self.amity.load_people('/Users/ahmedyusuf/desktop/person.txt')
+        NewSpaces = len(self.amity.all_allocations)
+        self.assertEqual(CurrentSpaces + 1, NewSpaces, msg="Loading failed")
 
-    def test_print_allocations(self):
-        pass
-
-    def test_check_for_proper_output(self):
-        pass
-
-    def test_print_unallocated(self):
-        pass
-
-    def test_check_for_proper_output(self):
-        pass
-
-    def test_print_room(self):
-        pass
+    # def test_print_unallocated(self):
+    #     pass
+    #
+    # def test_check_for_proper_output(self):
+    #     pass
+    #
+    # def test_print_room(self):
+    #     pass
 
 
 
--- controls/amity_controls.py
+++ controls/amity_controls.py
@@ -6,7 +6,12 @@ class AmityControls():
     def __init__(self):
         self.all_persons = {"Ahmed Yusuf":"Fellow", "Yusuf":"Staff"}
         self.all_rooms = {"Kigali":"Office","Krypton":"Living Space","Kampala":"Office","Gotham":"Living Space"}
-        self.all_allocations = {"Ahmed Yusuf":["Kigali","Krypton"],"Yusuf":["Kampala"]}
+        self.all_allocations = {"Ahmed Yusuf":["Kigali","Krypton"],"Yusuf Sebit":["Kampala"],"T MK":["Kampala","Gotham"]}
+        self.selected_room = {}
+
+    def create_room(self, room_name, room_type):
+        self.all_rooms.update({room_name:room_type})
+        pass
 
     def add_person(self, fname, lname, role, accom):
         name = fname+ " "+lname
@@ -17,25 +22,24 @@ class AmityControls():
         livingspace = random.choice(list(livingspaces))
         self.all_allocations.update({name:[office,livingspace]})
 
-
-    def create_room(self, room_name, room_type):
-        self.all_rooms.update({room_name:room_type})
-        pass
-
     def relocate_office(self, fname, lname ,reloc_type, room_name ):
         person = fname + " " + lname
-        self.all_allocations[person][1] = room_name
-        pass
+        #self.all_allocations[person][1] = room_name
 
-    def load_people(self):
-        f = open('/Users/ahmedyusuf/desktop/CP1AOfficeSpaceAllocation/officespaceallocation/files/person.txt','r')
+    def load_people(self, text_input):
+        f = open(text_input,'r')
         dct = {}
         count = 0
         for line in f:
             item = line.split()
-            self.add_person(item[0], item[1], item[2], item[3])
-        print self.all_persons
-        print self.all_allocations
+            #self.add_person(item[0], item[1], item[2], item[3])
+            return self.all_allocations
 
-amity = AmityControls()
-amity.load_people()
+    def print_room(self, name):
+        selected_room = {k: v for k, v in self.all_allocations.items() if v[0] == name}
+        return selected_room
+
+
+
+# amity = AmityControls()
+# amity.load_people('/Users/ahmedyusuf/desktop/CP1AOfficeSpaceAllocation/officespaceallocation/files/person.txt')
